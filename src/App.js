48583ecd23f926a2a5fe99e748202730b3d00aed
Harsh <harsh.rayzada@neutrinos.co>
2020-01-15 16:32:29 +0530

    updating the title using useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-// const App = () => {
-//     const [counter, setCounter] = useState(0);
+const App = () => {
+    const [counter, setCounter] = useState(0);
 
-//     const increment = () => {
-//         setCounter(counter + 1);
-//     }
-
-//     return (
-//         <div>
-//             <p>{counter}</p>
-//             <button onClick={increment}>Add</button>
-//         </div>
-//     )
-// };
-
-class App extends React.Component {
-  state = {
-    counter: 0
-  };
-
-  add = () => {
-    this.setState({
-      counter: this.state.counter + 1
+    useEffect(() => {
+      document.title = `${counter} times`
     });
-  }
 
-  componentDidMount(){
-    document.title = `${this.state.counter} times`;
-  }
+    const increment = () => {
+        setCounter(counter + 1);
+    }
 
-  componentDidUpdate(){
-    document.title = `${this.state.counter} times`;
-  }
+    return (
+        <div>
+            <p>{counter}</p>
+            <button onClick={increment}>Add</button>
+        </div>
+    )
+};
 
-  render() {
-    return <div>
-      <p>{this.state.counter}</p>
-      <button onClick={this.add}>Add</button>
-    </div>;
-  }
-}
+// class App extends React.Component {
+//   state = {
+//     counter: 0
+//   };
+
+//   add = () => {
+//     this.setState({
+//       counter: this.state.counter + 1
+//     });
+//   }
+
+//   componentDidMount(){
+//     document.title = `${this.state.counter} times`;
+//   }
+
+//   componentDidUpdate(){
+//     document.title = `${this.state.counter} times`;
+//   }
+
+//   render() {
+//     return <div>
+//       <p>{this.state.counter}</p>
+//       <button onClick={this.add}>Add</button>
+//     </div>;
+//   }
+// }
 
 export default App;
