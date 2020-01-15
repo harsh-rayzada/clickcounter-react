87a7398ff0984c087f954b6a84355c45a5fca116
Harsh <harsh.rayzada@neutrinos.co>
2020-01-15 16:16:08 +0530

    doing the same thing using the useState hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class App extends React.Component {
-  state = {
-    counter: 0
-  };
+const App = () => {
+    const [counter, setCounter] = useState(0);
 
-  add = () => {
-    this.setState({
-      counter: this.state.counter + 1
-    });
-  }
+    const increment = () => {
+        setCounter(counter + 1);
+    }
+
+    return (
+        <div>
+            <p>{counter}</p>
+            <button onClick={increment}>Add</button>
+        </div>
+    )
+};
+
+// class App extends React.Component {
+//   state = {
+//     counter: 0
+//   };
+
+//   add = () => {
+//     this.setState({
+//       counter: this.state.counter + 1
+//     });
+//   }
   
-  render() {
-  return <div>
-    <p>{this.state.counter}</p>
-    <button onClick={this.add}>Add</button>
-    </div>;
-  }
-}
+//   render() {
+//   return <div>
+//     <p>{this.state.counter}</p>
+//     <button onClick={this.add}>Add</button>
+//     </div>;
+//   }
+// }
 
 export default App;
